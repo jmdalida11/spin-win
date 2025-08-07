4fd57b013eb9f77e86e0d9ac6b66fcf4764ffede
John Mark Dalida <j.dalida@samsung.com>
2025-08-07 23:13:23 +0800

    Add upload logo

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -10,7 +10,7 @@ const Container = styled.div`
     margin-bottom: 10px;
   }
 
-   @media (max-width: 600px) {
+  @media (max-width: 600px) {
     display: block;
   }
 `;
@@ -45,19 +45,19 @@ const Card = styled.div`
 `;
 
 const Item = styled.div`
-  background-color: #F2E5BF;
+  background-color: #f2e5bf;
   height: 50px;
   width: 95%;
   display: inline-flex;
   align-items: center;
   justify-content: space-between;
   margin-bottom: 10px;
-  box-shadow: 0 0 1rem rgba(0,0,0,.6);
+  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.6);
   border-radius: 5px;
   padding-left: 10px;
   padding-right: 10px;
   font-weight: bold;
-  
+
   & > span {
     text-overflow: ellipsis;
     overflow: hidden;
@@ -70,21 +70,46 @@ const Item = styled.div`
 `;
 
 const InputWithLabel: React.FC<{
-   label: string; 
-   value: string;
-   setValue: (v: string) => void;
+  label: string;
+  value: string;
+  setValue: (v: string) => void;
 }> = ({ label, value, setValue }) => {
-  return <div className="inputWithLabel">
-    <label>{label}</label> <br />
-    <input value={value} onChange={(e) => { setValue(e.target.value) }} />
-  </div>
-}
+  return (
+    <div className="inputWithLabel">
+      <label>{label}</label> <br />
+      <input
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+    </div>
+  );
+};
+
+const readFileContent = async (file: File) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    reader.onload = (event: any) => {
+      resolve(event.target.result);
+    };
+
+    reader.onerror = (error) => {
+      reject(error);
+    };
+
+    reader.readAsDataURL(file);
+  });
+};
 
 const AdminPage = () => {
-  const [name, setName] = useState('');
-  const [filteredName, setFilteredName] = useState('');
+  const [name, setName] = useState("");
+  const [filteredName, setFilteredName] = useState("");
   const [players, setPlayers] = useState(loadPlayers());
   const [color, setColor] = useState(loadWheelColor());
+  const [logo, setLogo] = useState<string | null>(null);
   const textFieldPlayersRef = useRef(null);
   const textFieldPrizesRef = useRef(null);
 
@@ -92,7 +117,9 @@ const AdminPage = () => {
     if (!textFieldPlayersRef.current) {
       return;
     }
-    (textFieldPlayersRef.current as HTMLSelectElement).value = players.reduce((prev, cur) => prev + '\n' + cur.name, '').trim();
+    (textFieldPlayersRef.current as HTMLSelectElement).value = players
+      .reduce((prev, cur) => prev + "\n" + cur.name, "")
+      .trim();
   }, [players]);
 
   useEffect(() => {
@@ -100,7 +127,12 @@ const AdminPage = () => {
       return;
     }
     const prizes = loadPrizes();
-    (textFieldPrizesRef.current as HTMLSelectElement).value = prizes.reduce((prev, cur) => prev + '\n' + cur, '').trim();
+    (textFieldPrizesRef.current as HTMLSelectElement).value = prizes
+      .reduce((prev, cur) => prev + "\n" + cur, "")
+      .trim();
+
+    const logoData = localStorage.getItem("logo");
+    setLogo(logoData);
   }, []);
 
   const handleAddClick = () => {
@@ -110,110 +142,160 @@ const AdminPage = () => {
     });
     setPlayers([...players]);
     localStorage.setItem(StorageName.Players, JSON.stringify(players));
-    setName('');
-  }
+    setName("");
+  };
 
   const handleRemovePlayer = (id: string) => () => {
     const filteredPlayers = players.filter((p) => p.id !== id);
     setPlayers(filteredPlayers);
     localStorage.setItem(StorageName.Players, JSON.stringify(filteredPlayers));
-  }
+  };
 
   const handleAddPlayers = () => {
     if (!textFieldPlayersRef.current) {
       return;
     }
-    if (confirm('Current players will be replace with this new players. Proceed?')) {
+    if (
+      confirm("Current players will be replace with this new players. Proceed?")
+    ) {
       const value = (textFieldPlayersRef.current as HTMLSelectElement).value;
       const lines = value.split(/\r\n|\n|\r/);
       const newPlayers: Player[] = [];
       for (const line of lines) {
-        const playerName = line.replace(/\t/, ' ').trim();
-        if (playerName !== '') {
+        const playerName = line.replace(/\t/, " ").trim();
+        if (playerName !== "") {
           newPlayers.push({
             id: crypto.randomUUID(),
-            name: playerName
+            name: playerName,
           });
         }
       }
       localStorage.setItem(StorageName.Players, JSON.stringify(newPlayers));
       setPlayers(newPlayers);
     }
-  }
+  };
 
   const handleUpdatePrizes = () => {
     if (!textFieldPrizesRef.current) {
       return;
     }
-    if (confirm('Current prizes will be replace with this new prizes. Proceed?')) {
+    if (
+      confirm("Current prizes will be replace with this new prizes. Proceed?")
+    ) {
       const value = (textFieldPrizesRef.current as HTMLSelectElement).value;
       const lines = value.split(/\r\n|\n|\r/);
       const newPrizes: string[] = [];
       for (const line of lines) {
-        const prize = line.replace(/\t/, ' ').trim();
-        if (prize !== '') {
+        const prize = line.replace(/\t/, " ").trim();
+        if (prize !== "") {
           newPrizes.push(prize);
         }
       }
       localStorage.setItem(StorageName.Prizes, JSON.stringify(newPrizes));
     }
-  }
+  };
 
   const handleUpdateColor = () => {
-    if (CSS.supports('color', color)) {
+    if (CSS.supports("color", color)) {
       localStorage.setItem(StorageName.WheelColor, color);
-      alert('Wheel Color Successfully Updated!');
+      alert("Wheel Color Successfully Updated!");
     } else {
-      alert('Invalid color: ' + color);
+      alert("Invalid color: " + color);
     }
-  }
+  };
 
-  const playerList = players.filter((p) => p.name.toLowerCase().includes(filteredName.toLowerCase()))
-    .map((player) => <Item key={player.id}>
-    <span>{player.name}</span>
-    <button onClick={handleRemovePlayer(player.id)}>Remove</button>
-  </Item>)
+  const playerList = players
+    .filter((p) => p.name.toLowerCase().includes(filteredName.toLowerCase()))
+    .map((player) => (
+      <Item key={player.id}>
+        <span>{player.name}</span>
+        <button onClick={handleRemovePlayer(player.id)}>Remove</button>
+      </Item>
+    ));
 
-  return <Container>
-    <Left>
-      <fieldset>
-        <legend>Players</legend>
-        <textarea style={{ width: '98%' }} rows={5} ref={textFieldPlayersRef} />
-        <button onClick={handleAddPlayers}>Update Players</button>
-      </fieldset>
-      <br />
-      <fieldset>
-        <legend>Add New Player</legend>
-        <InputWithLabel label="Player name" value={name} setValue={setName} />
-        <button onClick={handleAddClick} disabled={name === ''}>Add</button>
-      </fieldset>
-      <br />
-      <fieldset>
-        <legend>Search Players</legend>
-        <InputWithLabel label="Player name" value={filteredName} setValue={setFilteredName} />
-      </fieldset>
-      <br />
-      <fieldset>
-        <legend>Update Prizes</legend>
-        <textarea style={{ width: '98%' }} rows={5} ref={textFieldPrizesRef} />
-        <button onClick={handleUpdatePrizes}>Update Prizes</button>
-      </fieldset>
-      <fieldset>
-        <legend>Update Wheel color</legend>
-        <InputWithLabel label="Color" value={color} setValue={setColor} />
-        <button onClick={handleUpdateColor}>Update Color</button>
-      </fieldset>
-    </Left>
-    <Right>
-      <Card>
-        <div style={{ width: '100%' }}>
-          {playerList}
-          {players.length > 0 && playerList.length === 0 && 'No player found.'}
-          {players.length === 0 && 'No players registered yet.'}
-        </div>
-      </Card>
-    </Right>
-  </Container>
-}
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const uploadLogo = (e: any) => {
+    (async () => {
+      try {
+        const file = e.target.files[0];
+        const base64String = (await readFileContent(file)) as string;
+        localStorage.setItem("logo", base64String);
+        setLogo(base64String);
+        alert("Logo successfully updated!");
+      } catch {
+        alert("Failed to upload the logo");
+      }
+    })();
+  };
+
+  return (
+    <Container>
+      <Left>
+        <fieldset>
+          <legend>Players</legend>
+          <textarea
+            style={{ width: "98%" }}
+            rows={5}
+            ref={textFieldPlayersRef}
+          />
+          <button onClick={handleAddPlayers}>Update Players</button>
+        </fieldset>
+        <br />
+        <fieldset>
+          <legend>Add New Player</legend>
+          <InputWithLabel label="Player name" value={name} setValue={setName} />
+          <button onClick={handleAddClick} disabled={name === ""}>
+            Add
+          </button>
+        </fieldset>
+        <br />
+        <fieldset>
+          <legend>Search Players</legend>
+          <InputWithLabel
+            label="Player name"
+            value={filteredName}
+            setValue={setFilteredName}
+          />
+        </fieldset>
+        <br />
+        <fieldset>
+          <legend>Update Prizes</legend>
+          <textarea
+            style={{ width: "98%" }}
+            rows={5}
+            ref={textFieldPrizesRef}
+          />
+          <button onClick={handleUpdatePrizes}>Update Prizes</button>
+        </fieldset>
+        <fieldset>
+          <legend>Update Wheel color</legend>
+          <InputWithLabel label="Color" value={color} setValue={setColor} />
+          <button onClick={handleUpdateColor}>Update Color</button>
+        </fieldset>
+        <fieldset>
+          <legend>Update Logo</legend>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={uploadLogo}
+            style={{ marginBottom: 5 }}
+          />
+          {logo !== null && <img src={logo} height={100} />}
+        </fieldset>
+      </Left>
+      <Right>
+        <Card>
+          <div style={{ width: "100%" }}>
+            {playerList}
+            {players.length > 0 &&
+              playerList.length === 0 &&
+              "No player found."}
+            {players.length === 0 && "No players registered yet."}
+          </div>
+        </Card>
+      </Right>
+    </Container>
+  );
+};
 
 export default AdminPage;
--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,11 +1,16 @@
 import styled from "@emotion/styled";
 import Spinner from "../components/Spinner";
-import Confetti from 'react-confetti';
+import Confetti from "react-confetti";
 import { useEffect, useMemo, useState } from "react";
-import { loadPlayers, loadPrizes, loadWheelColor, loadWinners, randomNumber } from "../utils";
-import SpinAudio from '../assets/spin.mp3';
+import {
+  loadPlayers,
+  loadPrizes,
+  loadWheelColor,
+  loadWinners,
+  randomNumber,
+} from "../utils";
+import SpinAudio from "../assets/spin.mp3";
 import { Player, StorageName, Winner } from "../types/types";
-import Logo from "../assets/logo.png";
 
 const Container = styled.div`
   width: 100%;
@@ -40,8 +45,8 @@ const SpinButton = styled.button`
   cursor: pointer;
 `;
 
-const Winners = styled.div<{color: string}>`
-  background-color: ${({ color }) => color ?? '#3498db'};
+const Winners = styled.div<{ color: string }>`
+  background-color: ${({ color }) => color ?? "#3498db"};
   padding: 5px;
   width: 90%;
   display: inline-flex;
@@ -56,11 +61,11 @@ const Winners = styled.div<{color: string}>`
 
 const audio = new Audio(SpinAudio);
 const defaultNames = [
-  'Enter Player',
-  'Enter Player',
-  'Enter Player',
-  'Enter Player',
-  'Enter Player',
+  "Enter Player",
+  "Enter Player",
+  "Enter Player",
+  "Enter Player",
+  "Enter Player",
 ];
 
 function Play() {
@@ -68,10 +73,13 @@ function Play() {
   const [players, setPlayers] = useState<Player[]>([]);
   const [winners, setWinners] = useState<Winner[]>([]);
   const [prizes, setPrizes] = useState<string[]>([]);
-  const [prize, setPrize] = useState('');
-  const [winnerIndex, setWinnerIndex] = useState(randomNumber(Math.max(players.length, 1)));
+  const [prize, setPrize] = useState("");
+  const [winnerIndex, setWinnerIndex] = useState(
+    randomNumber(Math.max(players.length, 1))
+  );
   const [isInputPrize, setIsInputPrize] = useState(false);
-  const [inputPrize, setInputPrize] = useState('');
+  const [inputPrize, setInputPrize] = useState("");
+  const [logo, setLogo] = useState<string | null>(null);
 
   const winnersColor = useMemo(() => loadWheelColor(), []);
 
@@ -80,14 +88,17 @@ function Play() {
     setWinners(loadWinners());
     setPrizes(loadPrizes());
 
+    const logoData = localStorage.getItem("logo");
+    setLogo(logoData);
+
     return () => {
       audio.pause();
       audio.currentTime = 0;
     };
   }, []);
-  
+
   useEffect(() => {
-    setPrize(prizes[0] ?? '');
+    setPrize(prizes[0] ?? "");
   }, [prizes]);
 
   const getTopPlayers = () => {
@@ -97,46 +108,55 @@ function Play() {
       return topNames;
     }
 
-    if (winnerIndex-2 < 0) {
-      topNames[0] = `${players[Math.max(players.length-2, 0)].name}`;
+    if (winnerIndex - 2 < 0) {
+      topNames[0] = `${players[Math.max(players.length - 2, 0)].name}`;
     } else {
-      topNames[0] = `${players[winnerIndex-2].name}`
+      topNames[0] = `${players[winnerIndex - 2].name}`;
     }
-    if (winnerIndex-1 < 0) {
-      topNames[1] = `${players[players.length-1].name}`
+    if (winnerIndex - 1 < 0) {
+      topNames[1] = `${players[players.length - 1].name}`;
     } else {
-      topNames[1] = `${players[winnerIndex-1].name}`;
+      topNames[1] = `${players[winnerIndex - 1].name}`;
     }
     topNames[2] = `${players[winnerIndex].name}`;
-    topNames[3] = `${players[(winnerIndex+1)%players.length].name}`;
-    topNames[4] = `${players[(winnerIndex+2)%players.length].name}`;
+    topNames[3] = `${players[(winnerIndex + 1) % players.length].name}`;
+    topNames[4] = `${players[(winnerIndex + 2) % players.length].name}`;
 
     return topNames;
-  }
+  };
 
   const removeWinner = (id: string) => () => {
     const filteredWinners = winners.filter((w) => w.id !== id);
     setWinners(filteredWinners);
     localStorage.setItem(StorageName.Winners, JSON.stringify(filteredWinners));
-  }
+  };
 
   const clearAllWinners = () => {
-    if (confirm('Are you sure you want to delete all the winners?')) {
+    if (confirm("Are you sure you want to delete all the winners?")) {
       setWinners([]);
-      localStorage.setItem(StorageName.Winners, '[]');
+      localStorage.setItem(StorageName.Winners, "[]");
     }
-  }
+  };
 
   const removePlayer = () => {
     if (players.length > 0) {
-      if (confirm(`Are you sure you want to remove "${players[winnerIndex].name}"?`)) {
-        const filteredPlayers = players.filter((p) => p.id !== players[winnerIndex].id);
+      if (
+        confirm(
+          `Are you sure you want to remove "${players[winnerIndex].name}"?`
+        )
+      ) {
+        const filteredPlayers = players.filter(
+          (p) => p.id !== players[winnerIndex].id
+        );
         setPlayers(filteredPlayers);
-        localStorage.setItem(StorageName.Players, JSON.stringify(filteredPlayers));
+        localStorage.setItem(
+          StorageName.Players,
+          JSON.stringify(filteredPlayers)
+        );
         setWinnerIndex(randomNumber(players.length));
       }
     }
-  }
+  };
 
   const topPlayers = getTopPlayers();
 
@@ -154,7 +174,14 @@ function Play() {
         index = index % players.length;
       } else {
         if (players.length > 0) {
-          const newWinners = [...winners, { ...players[index], id: crypto.randomUUID(), prize: isInputPrize ? inputPrize : prize }];
+          const newWinners = [
+            ...winners,
+            {
+              ...players[index],
+              id: crypto.randomUUID(),
+              prize: isInputPrize ? inputPrize : prize,
+            },
+          ];
           setWinners(newWinners);
           localStorage.setItem(StorageName.Winners, JSON.stringify(newWinners));
         }
@@ -165,7 +192,7 @@ function Play() {
         }, 5000);
       }
     }, randomNumber(60) + 60);
-  }
+  };
 
   const shuffle = () => {
     if (players.length > 0) {
@@ -173,60 +200,104 @@ function Play() {
         const swapPlayerIndex = randomNumber(players.length);
         const tempPlayer = players[i];
         players[i] = players[swapPlayerIndex];
-        players[swapPlayerIndex] = tempPlayer; 
+        players[swapPlayerIndex] = tempPlayer;
       }
       setPlayers([...players]);
       setWinnerIndex(randomNumber(players.length));
     }
-  }
+  };
 
-  return <Container>
-    <Left>
-      <div>
-        <img src={Logo} height={100} />
-        <Spinner names={topPlayers} />
-        <SpinButton disabled={players.length === 0 || !audio.paused} onClick={handleSpin}>Spin it!</SpinButton>
-        <SpinButton disabled={players.length === 0 || !audio.paused} onClick={shuffle}>Shuffle it!</SpinButton>
-        <SpinButton
-          style={{ color: 'red' }} 
-          disabled={players.length === 0 || !audio.paused} 
-          onClick={removePlayer}>Remove Player</SpinButton>
-      </div>
-    </Left>
-    <Right>
-      <div>
+  return (
+    <Container>
+      <Left>
+        <div>
+          <div style={{ height: 100, marginBottom: 5 }}>
+            {logo !== null && <img src={logo} height={100} />}
+          </div>
+          <Spinner names={topPlayers} />
+          <SpinButton
+            disabled={players.length === 0 || !audio.paused}
+            onClick={handleSpin}
+          >
+            Spin it!
+          </SpinButton>
+          <SpinButton
+            disabled={players.length === 0 || !audio.paused}
+            onClick={shuffle}
+          >
+            Shuffle it!
+          </SpinButton>
+          <SpinButton
+            style={{ color: "red" }}
+            disabled={players.length === 0 || !audio.paused}
+            onClick={removePlayer}
+          >
+            Remove Player
+          </SpinButton>
+        </div>
+      </Left>
+      <Right>
+        <div>
           <b>Prize: </b>
-          <select value={prize} onChange={(e) => setPrize(e.target.value)} disabled={isInputPrize}>
-            {prizes.map((v) => <option value={v}>{v}</option>)}
+          <select
+            value={prize}
+            onChange={(e) => setPrize(e.target.value)}
+            disabled={isInputPrize}
+          >
+            {prizes.map((v) => (
+              <option value={v}>{v}</option>
+            ))}
           </select>
           <span style={{ marginLeft: 20 }}>
             <b>Input Prize</b>
-            <input type="checkbox" checked={isInputPrize} onChange={() => setIsInputPrize(prev => !prev)} />
-            {isInputPrize && <input value={inputPrize} onChange={(e) => setInputPrize(e.target.value)} />}
+            <input
+              type="checkbox"
+              checked={isInputPrize}
+              onChange={() => setIsInputPrize((prev) => !prev)}
+            />
+            {isInputPrize && (
+              <input
+                value={inputPrize}
+                onChange={(e) => setInputPrize(e.target.value)}
+              />
+            )}
           </span>
-      </div>
-      <div>
-        <h4 style={{ display: 'inline-flex', justifyContent: 'space-between', width: '92%' }}>
-          <span>Winners:</span>
-          <span><button onClick={clearAllWinners}>Clear all winners</button></span>
-        </h4>
-        <div>
-          {winners.map((w, idx) => 
-          <Winners color={winnersColor}>
-            <b>{idx+1}. {w.name} (Prize: {w.prize})</b>
-            <button onClick={removeWinner(w.id)}>Remove</button>
-          </Winners>)}
-          
         </div>
-      </div>
-    </Right>
-    {runConfetti && <Confetti
-      numberOfPieces={500}
-      recycle={false}
-      width={window.innerWidth - (window.innerWidth * 0.05)}
-      height={window.innerHeight - (window.innerHeight * 0.05)}
-    />}
-  </Container>
+        <div>
+          <h4
+            style={{
+              display: "inline-flex",
+              justifyContent: "space-between",
+              width: "92%",
+            }}
+          >
+            <span>Winners:</span>
+            <span>
+              <button onClick={clearAllWinners}>Clear all winners</button>
+            </span>
+          </h4>
+          <div>
+            {winners.map((w, idx) => (
+              <Winners color={winnersColor}>
+                <b>
+                  {idx + 1}. {w.name} (Prize: {w.prize})
+                </b>
+                <button onClick={removeWinner(w.id)}>Remove</button>
+              </Winners>
+            ))}
+          </div>
+        </div>
+      </Right>
+      {runConfetti && (
+        <Confetti
+          numberOfPieces={500}
+          recycle={false}
+          width={window.innerWidth - window.innerWidth * 0.05}
+          height={window.innerHeight - window.innerHeight * 0.05}
+        />
+      )}
+    </Container>
+  );
 }
 
 export default Play;
